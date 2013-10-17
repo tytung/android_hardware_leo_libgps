0c83abc999f5893ad2c6b8e8ed35970d6f2ede95
walter79 <gzwalter79@gmail.com>
2013-10-17 15:34:45 +0200

    replace LOGE and LOGD with LOGx

--- leo-gps-rpc.c
+++ leo-gps-rpc.c
@@ -44,7 +44,7 @@
 #define  GPS_DEBUG  0
 
 #if GPS_DEBUG
-#  define  D(...)   LOGD(__VA_ARGS__)
+#  define  D(...)   LOGx(__VA_ARGS__)
 #else
 #  define  D(...)   ((void)0)
 #endif
@@ -922,11 +922,11 @@ int parse_gps_conf() {
         }
     }
     fclose(file);
-    LOGD("%s() is called: GPS1_XTRA_AUTO_DOWNLOAD_ENABLED = %d", __FUNCTION__, XTRA_AUTO_DOWNLOAD_ENABLED);
-    LOGD("%s() is called: GPS1_XTRA_DOWNLOAD_INTERVAL = %d", __FUNCTION__, XTRA_DOWNLOAD_INTERVAL);
-    LOGD("%s() is called: GPS1_CLEANUP_ENABLED = %d", __FUNCTION__, CLEANUP_ENABLED);
-    LOGD("%s() is called: GPS1_SESSION_TIMEOUT = %d", __FUNCTION__, SESSION_TIMEOUT);
-    LOGD("%s() is called: GPS1_MEASUREMENT_PRECISION = %d", __FUNCTION__, MEASUREMENT_PRECISION);
+    LOGx("%s() is called: GPS1_XTRA_AUTO_DOWNLOAD_ENABLED = %d", __FUNCTION__, XTRA_AUTO_DOWNLOAD_ENABLED);
+    LOGx("%s() is called: GPS1_XTRA_DOWNLOAD_INTERVAL = %d", __FUNCTION__, XTRA_DOWNLOAD_INTERVAL);
+    LOGx("%s() is called: GPS1_CLEANUP_ENABLED = %d", __FUNCTION__, CLEANUP_ENABLED);
+    LOGx("%s() is called: GPS1_SESSION_TIMEOUT = %d", __FUNCTION__, SESSION_TIMEOUT);
+    LOGx("%s() is called: GPS1_MEASUREMENT_PRECISION = %d", __FUNCTION__, MEASUREMENT_PRECISION);
     return 0;
 }
 
@@ -973,9 +973,9 @@ int init_leo()
     
     if (!CHECKED[0]) {
         if (use_nmea)
-            LOGD("%s() is called: %s version", __FUNCTION__, "NMEA");
+            LOGx("%s() is called: %s version", __FUNCTION__, "NMEA");
         else
-            LOGD("%s() is called: %s version", __FUNCTION__, "RPC");
+            LOGx("%s() is called: %s version", __FUNCTION__, "RPC");
 
         parse_gps_conf();
         if (XTRA_AUTO_DOWNLOAD_ENABLED)
